58272e86b0f16c7dc49c3c177bb7e179ad71b674
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2020-12-22 18:59:41 +0100

    Data and Types

--- typeDefs.js
+++ typeDefs.js
@@ -1,14 +1,33 @@
 const { gql } = require('apollo-server');
 const typeDefs = gql`
-    schema {
-        query: Query
-    }
-    type Query {
-        books: [Book]
-    }
-    type Book {
-        title: String
-    }
+schema {
+    query: Query
+  }
+  type Query {
+    authors: [Author!]!
+    books: [Book!]!
+    users: [User!]!
+  }
+  type Author {
+    name: String!
+    photo: Image!
+  }
+  type Book {
+    title: String!
+    cover: Image!
+  }
+  type User {
+    name: String!
+    email: String!
+    avatar: Avatar!
+  }
+  type Image {
+    url: String!
+  }
+  type Avatar {
+    image: Image!
+    color: String!
+  }
 `;
 
 module.exports = typeDefs;
