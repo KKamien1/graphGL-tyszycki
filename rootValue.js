450704515d0c2614d7b4f95635f1173256409c49
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2021-01-04 17:09:39 +0100

    2.12 differences of data structures

--- rootValue.js
+++ rootValue.js
@@ -1,5 +1,7 @@
 const db = require("./db");
 
+const allBooks = db.getAllBooks();
+console.log('allBooks', allBooks)
 const book = db.getBookById(2);
 console.log("Book #2", book);
 const author = db.getAuthorById(book.authorId);
