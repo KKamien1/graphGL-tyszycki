3bb507048ccd1bf873fa6f96abc0d2fa59c3ccf0
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2021-02-15 12:05:04 +0100

    Coded ids

--- resolvers.js
+++ resolvers.js
@@ -1,19 +1,25 @@
+
+const toDbId = externalId => Buffer.from(externalId, 'base64').toString();
+const toExternalId =  (dbId) => Buffer.from(dbId).toString('base64');
+
 const resolvers = {
   Query: {
     books: (rootValue, args, { db }) => db.getAllBooks(),
     authors: (rootValue, args, { db }) => db.getAllAuthors(),
     users: (rootValue, args, { db }) => db.getAllUsers(),
-    book: (rootValue, { id }, { db }) => db.getBookById(id),
-    user: (rootValue, { id }, { db }) => db.getUserById(id),
-    author: (rootValue, { id }, { db }) => db.getAuthorById(id)
+    book: (rootValue, { id }, { db }) => db.getBookById(toDbId(id)),
+    user: (rootValue, { id }, { db }) => db.getUserById(toDbId(id)),
+    author: (rootValue, { id }, { db }) => db.getAuthorById(toDbId(id))
   },
   Book: {
+    id: book => toExternalId(book.id),
     author: (book, args, { db }) => db.getAuthorById(book.authorId),
     cover: book => ({
       path: book.coverPath
     })
   },
   Author: {
+    id: author => toExternalId(author.id),
     books: (author, args, { db }) => author.bookIds.map(db.getBookById),
     photo: author => ({
       path: author.photoPath
@@ -26,6 +32,10 @@ const resolvers = {
   },
   Image: {
     url: (image, args, { baseAssetsUrl }) => baseAssetsUrl + image.path
+  },
+  User: {
+    id: user => toExternalId(user.id),
+    
   }
 };
 
