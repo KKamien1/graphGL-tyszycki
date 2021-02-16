69cf6865e89ca15c60544acfccfb5b3f4a0a9229
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2021-02-16 11:22:24 +0100

    Lunr library for text search

--- search.js
+++ search.js
@@ -1,11 +1,28 @@
+const lunr = require("lunr");
+
+function initBooksIndex(db) {
+  return lunr(function() {
+    this.ref("id");
+    this.field("title", { boost: 10 });
+    this.field("description");
+    db.getAllBooks().forEach(function(book) {
+      this.add(book);
+    }, this);
+  });
+}
+
+
 class Search {
     constructor(db){
         this.db = db;
+        this.bookIndex = initBooksIndex(this.db);
 
     }
 
     findBooks(searchQuery) {
-        const foundIds = ["1", "12", "20"];
+        const result = this.bookIndex.search(searchQuery);
+        console.log(result);
+        const foundIds = result.map(res => res.ref);
         return foundIds.map(this.db.getBookById);
     }
 }
